d07b2c56949bebb2f156d2f97ac2739cbbf45f53
Margaret Farmer <margaret.farmer@gmail.com>
2022-01-28 15:56:30 -0500

    Edit twoNumberSum

--- solutions/arraySum-problems/twoNumberSum.js
+++ solutions/arraySum-problems/twoNumberSum.js
@@ -8,9 +8,9 @@ The target sum can only be found by adding two distinct integers together, not a
 // time complexity O(n^2)
 // space complexity O(n)
 function twoNumberSumLoop(arr, sum) {
-  for(let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] + arr[j] === sum) return true
+      if (arr[i] + arr[j] === sum) return true;
     }
   }
   return false;
@@ -20,37 +20,52 @@ function twoNumberSumLoop(arr, sum) {
 // time complexity O(n * log(n))
 // space complexity O(1)
 function twoNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+  let first = 0,
+    last = array.length - 1,
+    list = [];
 
-	array.sort((a,b) => a-b)
-	let first = 0, last = array.length-1, list = [];
-
-	while(first < last) {
-
-		if (array[first] + array[last] === targetSum) {
-			list.push(array[first], array[last]);
-			first++;
-			last--;
-		}
-		else if (array[first] + array[last] < targetSum) first++;
-		else last--;
-	}
-	return list;
+  while (first < last) {
+    if (array[first] + array[last] === targetSum) {
+      list.push(array[first], array[last]);
+      first++;
+      last--;
+    } else if (array[first] + array[last] < targetSum) first++;
+    else last--;
+  }
+  return list;
 }
 
-console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
-
+console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
 
 // solution using a hash table
 // time complexity O(n)
 // space complexity O(n)
 function twoNumberSumHash(array, target) {
-
-	let hash = {}, list = [];
-	for (let i = 0; i < array.length; i++) {
-		if (hash[target-array[i]]) list.push(array[i], target-array[i])
-		else hash[array[i]] = true;
-	}
-	return list;
+  let hash = {},
+    list = [];
+  for (let i = 0; i < array.length; i++) {
+    if (hash[target - array[i]]) list.push(array[i], target - array[i]);
+    else hash[array[i]] = true;
+  }
+  return list;
 }
 
-console.log(twoNumberSumHash([3, 5, -4, 8, 11, 1, -1, 6], 10))
+console.log(twoNumberSumHash([3, 5, -4, 8, 11, 1, -1, 6], 10));
+
+// YET ANOTHER SOLUTION using a has table
+// this one involves pushing the INDICES of the numbers that add up to the target
+// Which is why it needs to be specified that the value in a key-value pair could be zero or else it won't register
+// also for this Leetcode problem it was specified that there was only one correct pair.
+function twoSumHashTwo(nums, target) {
+  let hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let elem = nums[i];
+    let otherNum = target - elem;
+
+    if (hash[otherNum] || hash[otherNum] === 0) return [i, hash[otherNum]];
+    else hash[elem] = i;
+  }
+  return [];
+}
