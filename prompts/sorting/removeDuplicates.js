026cf0e49d9150423713d7870b342a6b380192b2
Margaret Farmer <margaretfarmer@Margarets-MBP.lan>
2022-02-12 19:14:42 -0500

    Add removeDuplicates

---
+++ prompts/sorting/removeDuplicates.js
@@ -0,0 +1,20 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+You cannot change the length of the array. You must have the result placed in the first part of the array nums. If there are k elements after removing duplicates, then the first k elements of nums shoul dhold the final result. It does not matter what fills the rest of the array.
+
+NO COPYING, new sets, or pushing onto new arrays.
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
